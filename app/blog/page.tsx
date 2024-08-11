d221fab7b30708ad23c5b5a30a78e0b7bfbb74ff
heromfabian <heromfabian@gmail.com>
2024-08-11 03:55:22 +0000

    poetic toast

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import LinkCard from '../../components/LinkedCard';
-
+import PoeticToast from '../../public/blog/a-poetic-toast.webp';
 const BlogPage: React.FC = () => {
     return (
         <div className="container mx-auto px-4 py-8">
@@ -8,7 +8,7 @@ const BlogPage: React.FC = () => {
             <LinkCard
                 title="A Poetic Toast"
                 summary="In this post, we immerse ourselves in literature, poetry, and wine. Dive deep into the works of a poet who has forever marked history."
-                imageUrl="https://example.com/imagen.jpg"
+                imageUrl={PoeticToast}
                 articleUrl="https://medium.com/p/59406d3b51e5"
             />
         </div>
--- components/LinkedCard/index.tsx
+++ components/LinkedCard/index.tsx
@@ -1,16 +1,24 @@
+import Image, { StaticImageData } from 'next/image';
 import React from 'react';
 
 interface LinkCardProps {
   title: string;
   summary: string;
-  imageUrl: string;
+  imageUrl: string | StaticImageData;
   articleUrl: string;
 }
 
 const LinkCard: React.FC<LinkCardProps> = ({ title, summary, imageUrl, articleUrl }) => {
   return (
     <div className="border border-gray-300 rounded-lg overflow-hidden shadow-lg hover:shadow-xl transition-shadow duration-300">
-      <img src={imageUrl} alt={`Imagen de ${title}`} className="w-full h-48 object-cover" />
+      <Image 
+        src={imageUrl} 
+        alt={`Imagen de ${title}`} 
+        className="w-full h-48 object-cover" 
+        layout="responsive" 
+        width={700} 
+        height={300} 
+      />
       <div className="p-4">
         <h3 className="text-lg font-semibold">{title}</h3>
         <p className="text-gray-600 mb-4">{summary}</p>
