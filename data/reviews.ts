5b7fd32df5ed7ed8b531187f486a79757ad84a72
Fabian <heromfabian@gmail.com>
2025-04-04 23:23:21 -0600

    Actualiza la sección de reseñas: se añaden nuevas reseñas de Erin Miller y Giovanny Telon, y se mejora la estructura del código al cambiar las comillas simples por dobles para mantener la consistencia. Se eliminan reseñas obsoletas y se ajusta el formato general.

--- data/reviews.ts
+++ data/reviews.ts
@@ -1,20 +1,32 @@
 export const reviews = [
-    {
-      id: 1,
-      author: 'Roberto García',
-      title: 'Chief Systems Engineer at EPAM Systems',
-      body: 'Fabian has consistently proved himself an engineer with a remarkable ability to tackle challenges of any scale. He is highly productive and demonstrates a keen aptitude for learning and applying new technologies to solve problems. His dynamic personality and positive attitude make working together an enjoyable experience every time. Throughout the assigned mentoring, Fabian has developed impressive expertise in multiple technologies, including Python, React, Linux, AWS, IaC, Docker, and serverless technologies. Fabian has consistently achieved challenging goals, demonstrating that he is a valuable asset to any team.',
-    },
-    {
-      id: 2,
-      author: 'Jose Sandoval',
-      title: 'Lead Systems Engineer at EPAM Systems | AWS Certified',
-      body: 'Fabian is an enthusiastic engineer who never stops learning new technologies. Accepts new challenges and has a high level of commitment to solve them. His skills have extended from front-end solutions to building a backend REST API based on cloud services. He also understands how CICD pipelines work and how to set up the cloud resources using IaC. Fabian has acquired the knowledge to quickly solve problems and extend or build new services. He is a resource with outstanding performance that will shine on any team.',
-    },
-    {
-      id: 3,
-      author: 'Linkedin',
-      title: 'Email',
-      body: 'If you have worked with me and would like to share your experience, please write me a testimonial on LinkedIn or send it to me via email at heromfabian@gmail.com! I would love to hear your feedback!',
-    },
-  ]
+  {
+    id: 1,
+    author: "Roberto García",
+    title: "Chief Systems Engineer at EPAM Systems",
+    body: "Fabian has consistently proved himself an engineer with a remarkable ability to tackle challenges of any scale. He is highly productive and demonstrates a keen aptitude for learning and applying new technologies to solve problems. His dynamic personality and positive attitude make working together an enjoyable experience every time. Throughout the assigned mentoring, Fabian has developed impressive expertise in multiple technologies, including Python, React, Linux, AWS, IaC, Docker, and serverless technologies. Fabian has consistently achieved challenging goals, demonstrating that he is a valuable asset to any team.",
+  },
+  {
+    id: 2,
+    author: "Jose Sandoval",
+    title: "Lead Systems Engineer at EPAM Systems | AWS Certified",
+    body: "Fabian is an enthusiastic engineer who never stops learning new technologies. Accepts new challenges and has a high level of commitment to solve them. His skills have extended from front-end solutions to building a backend REST API based on cloud services. He also understands how CICD pipelines work and how to set up the cloud resources using IaC. Fabian has acquired the knowledge to quickly solve problems and extend or build new services. He is a resource with outstanding performance that will shine on any team.",
+  },
+  {
+    id: 3,
+    author: "Erin Miller",
+    title: "UX | XR | Product Designer in B2B SaaS",
+    body: "I've had the pleasure of working with Fabian for a Natural Products platform, and his technical expertise, flexibility, and sense of humor have been extremely valuable to this project. He consistently turned complex design concepts into seamless, functional code while offering thoughtful suggestions to improve the user experience. Fabian's collaborative approach and attention to detail make him an exceptional developer to work with!",
+  },
+  {
+    id: 4,
+    author: "Giovanny Telon",
+    title: "Quality Assurance Engineer",
+    body: "Fabian is an excellent developer with extensive knowledge in React, NestJS, and AWS tools. He has a great ability to detect details and ensure the user experience aligns exactly with the design specifications.",
+  },
+  {
+    id: 5,
+    author: "LinkedIn",
+    title: "Email",
+    body: "If you have worked with me and would like to share your experience, please write me a testimonial on LinkedIn or send it to me via email at heromfabian@gmail.com! I would love to hear your feedback!",
+  },
+];
